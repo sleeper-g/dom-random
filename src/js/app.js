b063a4351f176cdee31eb8576620b986f2a0c390
sleeper-g <green.sleeper@gmail.com>
2024-06-07 23:27:22 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+import GridContainer from './grid-container/grid-container';
+
+const littleGrid = new GridContainer();
+littleGrid.generateGrid(3, 5)
---
+++ src/js/grid-container/grid-container.js
@@ -0,0 +1,19 @@
+export default class GridContainer {
+
+    constructor(){
+    }
+
+    generateGrid(rows, columns){
+        const container = document.querySelector('.grid-container');
+        for (let indexRow = 0; indexRow < rows; indexRow++){
+            for (let indexColumn = 0; indexColumn < columns; indexColumn++){
+                const item = document.createElement('div');
+                item.className = 'grid-item';
+                item.innerText = `${indexRow}-${indexColumn}`
+                container.appendChild(item);
+
+                document.addEventListener('DOMContentLoaded', () => {})
+            };
+        }; 
+    };
+};
